2b1874a4b9807b5f77a54e1461d383796e35a1ce
Younis3 <91804311+younis3@users.noreply.github.com>
2024-11-16 23:19:21 +0200

    sidebar setup

---
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { LockIcon } from "lucide-react";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const Sidebar = () => {
+  const [showProjects, setShowProjects] = useState(true);
+  const [showPriority, setShowPriority] = useState(true);
+
+  const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl transition-all duation-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64`;
+
+  return (
+    <div className={sidebarClassNames}>
+      <div className="flex h-[100%] w-full flex-col justify-start">
+        {/* Sidebar Logo */}
+        <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
+          <div className="text-xl font-bold text-gray-800 dark:text-white">
+            Sidebar
+          </div>
+        </div>
+        {/* Team */}
+        <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
+          <Image src="/logo.png" alt="logo" width={40} height={40} />
+
+          <div>
+            <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
+              Younis3 Team
+            </h3>
+            <div className="mt-1 flex items-start gap-2">
+              <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
+              <p className="text-xs text-gray-500">Private</p>
+            </div>
+          </div>
+        </div>
+        {/* Sidebar Items */}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* sidebar */}
-      sidebar
+      <Sidebar />
       <main className="dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64">
         {/* navbar */}
         <Navbar />
